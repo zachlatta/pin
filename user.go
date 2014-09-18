47b40ee9f604f2c85efdc69d19c3e9820adb3fea
Zach Latta <zach@zachlatta.com>
2014-09-18 11:29:19 -0700

    Add user.APIToken API call.

--- user.go
+++ user.go
@@ -11,14 +11,15 @@ type UserService struct {
 	client *Client
 }
 
+type xmlResult struct {
+	XMLName xml.Name `xml:"result"`
+	Body    string   `xml:",chardata"`
+}
+
 // SecretRSSKey returns the authenticated user's secret RSS key for viewing
 // private RSS feeds.
 func (s *UserService) SecretRSSKey() (string, *http.Response, error) {
-	var result struct {
-		XMLName xml.Name `xml:"result"`
-		Secret  string   `xml:",chardata"`
-	}
-
+	result := xmlResult{}
 	req, err := s.client.NewRequest("user/secret", nil)
 	if err != nil {
 		return "", nil, err
@@ -29,5 +30,21 @@ func (s *UserService) SecretRSSKey() (string, *http.Response, error) {
 		return "", nil, err
 	}
 
-	return result.Secret, resp, nil
+	return result.Body, resp, nil
+}
+
+// APIToken returns the authenticated user's API token.
+func (s *UserService) APIToken() (string, *http.Response, error) {
+	result := xmlResult{}
+	req, err := s.client.NewRequest("user/api_token", nil)
+	if err != nil {
+		return "", nil, err
+	}
+
+	resp, err := s.client.Do(req, &result)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return result.Body, resp, nil
 }
